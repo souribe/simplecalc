7cc2cb4470d44fab7ca6c90f973a2dbd95dfe906
Benny Souriyadeth <bennys@uw.edu>
2018-01-12 22:58:44 -0800

    complete build for simplecalc test and for count

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -2,14 +2,54 @@
 //  main.swift
 //  SimpleCalc
 //
-//  Created by Ted Neward on 10/3/17.
-//  Copyright © 2017 Neward & Associates. All rights reserved.
+//  Created by Benny Souyadeth on 1/12/18.
+//  Copyright © 2017 Benny Souriyadeth. All rights reserved.
 //
 
 import Foundation
 
+/* There are two methods in here, one that takes an array of Strings as input, and one that takes a single String. The single String calculate takes the String and splits it into multiple Strings and passes that to the array-of-Strings version of calculate, so the bulk of your work will be in there. */
+
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        print(args)
+        var count : Int = 0
+        let input : [String] = args
+        for _ in input {
+            count += 1
+        }
+        if count == 3 {
+            let input1 : Int = Int(input[0])!
+            let input2: Int = Int(input[2])!
+            switch input[1] {
+                case "+":
+                    return (input1 + input2)
+                case "-":
+                    return (input1 - input2)
+                case "*":
+                    return (input1 * input2)
+                case "/":
+                    return (input1 / input2)
+                default:
+                    return (input1 % input2)
+            }
+        } else {
+            count = count - 1
+            switch input[count] {
+            case "count":
+                return count
+            case "avg":
+//                let total : Int = 0
+//                for num in input {
+//                    total +=
+//                }
+                return 2
+            case "fact":
+                return 3
+            default:
+                return 0
+            }
+        }
     }
     
     public func calculate(_ arg: String) -> Int {
